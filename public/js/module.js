cb55a59195d88860d390ac89c8ec622c7c461adb
Stephanie Zielinski <szielinski26@gmail.com>
2017-03-20 12:59:20 -0400

    thankyou

--- public/js/module.js
+++ public/js/module.js
@@ -1,41 +1,5 @@
 var app = angular.module('privyMod', ['ngRoute']);
 
-// app.config(function(uiGmapGoogleMapApiProvider) {
-//     uiGmapGoogleMapApiProvider.configure({
-//         key: 'AIzaSyDDSY7PloqKzlS8KIOrwnbmq2bnhuti1L8',
-//       //  v: '3.20', //defaults to latest 3.X anyhow
-//         libraries: 'weather,geometry,visualization,places'
-//
-//     });
-// })
-
-
-
-// app.directive('googleplace', function() {
-//     return {
-//         require: 'ngModel',
-//         link: function(scope, element, attrs, model) {
-//             var options = {
-//                 types: [],
-//                 componentRestrictions: {}
-//             };
-//             $scope.gPlace = new google.maps.places.PlacesService($scope.map);
-//
-//             google.maps.event.addListener($scope.gPlace, 'place_changed', function() {
-//                 scope.$apply(function() {
-//                     model.$setViewValue(element.val());
-//                 });
-//             });
-//         }
-//     };
-// });
-
-//myApp.factory('myService', function() {});
-//
-// function MyCtrl($scope) {
-//     $scope.gPlace;
-// }
-
 app.directive('privynav', function(){
   return {
     restrict: 'EA',
