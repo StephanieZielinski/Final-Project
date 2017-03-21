41f3439110609335d7f176c5c265571111f0b000
Stephanie Zielinski <szielinski26@gmail.com>
2017-03-21 09:52:41 -0400

    ty

--- public/js/factory.js
+++ public/js/factory.js
@@ -51,41 +51,7 @@ app.factory('privyService', function($http) {
     return promise;
   };
 
+}
 
-//trying to add google id to database
-  // function addId(id) {
-  //   var promise = $http({
-  //     method: 'POST',
-  //     url: '/addId',
-  //     data: resultsDisplay.id
-  //   }).then(function successCallback(response) {
-  //     console.log(response);
-  //     reviewList = response.data;
-  //   });
-  //   return promise;
-  // };
-
-
-  // $http({
-  //   method: 'GET',
-  //   url: '/locationreview'
-  //   }).then(function successCallback(response){
-  //   $scope.locationReview = response.data;
-  //   });
-  //
-  // $http({
-  //   method: 'GET',
-  //   url: '/results'
-  //   }).then(function successCallback(response) {
-  //   $scope.reviewList= response.data;
-  //   });
-  //
-  // $http({
-  //   method: 'GET',
-  //   url: '/select'
-  //   }).then(function successCallback(response){
-  //   // Need to match names selectBathroom or selectType or selectCatag
-  //   $scope.select = response.data;
-  //   });
 
 });
